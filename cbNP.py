dfc0d6f5b6f57ccdc71a01adbb4a20c71d9ec0f1
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2025-06-05 11:24:19 +0200

    v2.1.0

--- cbNP.py
+++ cbNP.py
@@ -102,7 +102,7 @@ class cbNPApp(rumps.App):
             rumps.MenuItem('Update manually', callback=self.update_manually),
             None,
             rumps.MenuItem('Preferences', callback=self.open_preferences),
-            "v2.0.0",
+            "v2.1.0",
             rumps.MenuItem('Quit', callback=self.exit_application)
         ]
 
