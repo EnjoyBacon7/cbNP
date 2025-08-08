4021a838fba90ee14ffc5ae457303f7d62b1787d
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2025-08-08 16:52:18 +0200

    Initial Commit

---
+++ cbNP/cbNPApp.swift
@@ -0,0 +1,17 @@
+//
+//  cbNPApp.swift
+//  cbNP
+//
+//  Created by Camille Bizeul on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct cbNPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
